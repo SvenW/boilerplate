8047b3df6e0e8febb78d01b6222031b5008a9c92
Sven Widen <sven.widen@gmail.com>
2013-04-05 10:17:19 +0200

    fix for grunt task

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 require.config({
   "paths": {
     "jquery": "vendor/jquery/jquery",
+    "backbone-amd": "vendor/backbone-amd/backbone",
+    "underscore-amd": "vendor/underscore-amd/underscore",
     "underscore": "vendor/underscore-amd/underscore",
     "backbone": "vendor/backbone-amd/backbone"
   }
